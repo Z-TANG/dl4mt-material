cdccb78454102dbf9bbf7eb32d1a2ace12c2566e
Z-TANG <tangzhiwen1994@163.com>
2016-03-08 12:07:14 +0100

    changed process number

--- session2/test.sh
+++ session2/test.sh
@@ -8,7 +8,7 @@
 export THEANO_FLAGS=device=cpu,floatX=float32
 
 #cd $PBS_O_WORKDIR
-python ./translate.py -n -p 8 \
+python ./translate.py -p 1 \
 	./model_hal.npz  \
 	../../data/train.en.tok.pkl \
 	../../data/train.de.tok.pkl \
